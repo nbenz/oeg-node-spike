8e388d2ab3c379f5ee62392f782307a91bd40a51
unknown <zdrivett@WC-002-12.ONU-AD.olivet.edu>
2015-11-24 11:13:33 -0600

    Changed parameters to points, removed tabs, removed unnecessary functions

--- map-manager.js
+++ map-manager.js
@@ -5,27 +5,12 @@ var elevationError[][][];
 
 
 function loadInitialSimulation(){
-	//load inital map (from saved JSON? we hope...)
-	//randomly generate some fudge values & store it
-	//for each user: change the value of their mask to be an empty 2d of the correct size
-	initializeFudge();
+  //load inital map (from saved JSON? we hope...)
+  //randomly generate some fudge values & store it
+  //for each user: change the value of their mask to be an empty 2d of the correct size
+  initializeFudge();
 }
 
-function initializeFudge(){
-	
-}
-
-
-//Make a mask object with functions that can update itself with functions
-//Then the team has maps...
-
-function updateMask(){//take in a user, cells you want updated...
-//given some cells, either:
-/*
-	1) fetch data from fudgeOffsets and store it in appropriate places
-	2) zeroe out part of the mask
-
-
-*/	
-	
+function initializeElevationErrors(){
+  
 }
--- mask-manager.js
+++ mask-manager.js
@@ -6,52 +6,53 @@ Part of a Team...the team constructs one of me, and updates it later as it sees 
 */
 var mask;
 
-function applySeismic(x, y){
-	//updates mask for a seismic request
-	if (!mask[x][y].drilled){	//If not drilled
-		mask[x][y].layerError = elevationError[x][y];
-	}
+function applySeismic(p){
+  //updates mask for a seismic request
+  if (!mask[p.x][p.y].drilled){	//If not drilled
+    mask[p.x][p.y].layerError = elevationError[p.x][p.y];
+  }
 }
 
 function initializeMask(size){
-	var outMask = [];
-	for (var i = 0; i < size; i++){
-		var row = [];
-		for (var j = 0; j < size; j++){
-			row.push({drilled: false});
-		}
-		outMask.push(row);
-	}
-	return outMask;
+  var outMask = [];
+  for (var i = 0; i < size; i++){
+    var row = [];
+    for (var j = 0; j < size; j++){
+	  row.push({drilled: false});
+    }
+    outMask.push(row);
+  }
+  return outMask;
 }
 
-function applyDrill(x, y){
-	//zero out fudge for specific x,y point. 
-	//(get rid of the 1d array there)
-	mask[x][y].drilled = true;
-	mask[x][y].layerError.length = 0;//empty out any data stored there. 
+function applyDrill(p){
+  //zero out fudge for specific x,y point. 
+  //(get rid of the 1d array there)
+  mask[p.x][p.y].drilled = true;
+  //later, we won't drill to the bottom
+  //drilled will be set to a depth/elevation/number of layers drilled into
+  mask[p.x][p.y].layerError.length = 0;//empty out any data stored there. 
 }
 
-function getCellInfo(x, y){
-	if (mask[x][y].drilled){
-		//return absolute truth 
-	}else if (mask.[x][y].elevationError){
-		//return skewed elevations
-	} else {
-		//return basic surface level info
-	}
-	
-	
-	
-	//for a given point:
-	//if mask is null there,
-		//return basic info 
-	//if there is an array there:
-		//look into map and grab values
-		//apply the appropriate offset for each
-		//return information (additional stuff is a 1d array of elevations and rocktypes)
-	//if there is a zero there
-		//return appropriate mask values without masking them
-		//again, this includes a 1d array of elevations and values.
-	
+function getCellInfo(p){
+  if (mask[p.x][p.y].drilled){//later, we will have this check to see if drilled is > or < a certain value
+                              //and return partial absolute truth (to a certain depth)
+  //return absolute truth 
+  }else if (mask[p.x][p.y].layerError){
+  //return skewed elevations
+  } else {
+  //return basic surface level info
+  }
+}
+
+  //for a given point:
+  //if mask is null there,
+    //return basic info 
+  //if there is an array there:
+    //look into map and grab values
+    //apply the appropriate offset for each
+    //return information (additional stuff is a 1d array of elevations and rocktypes)
+  //if there is a zero there
+    //return appropriate mask values without masking them
+    //again, this includes a 1d array of elevations and values.
 }
